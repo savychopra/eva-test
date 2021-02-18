8f57da73aba91eed00a5cbd0ff83574abfd96e01
shivalik-chopra <shivalik.chopra@hytechpro.com>
2021-02-18 13:14:19 +0530

    initial

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -796,7 +796,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'vc-git-new',
+  'database' => 'vc-git-new2',
   'username' => 'root',
   'password' => 'savy',
   'prefix' => '',
